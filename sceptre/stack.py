55a32b272301c6fbb7d91c6e0a2efc42b3d8f96e
oazmon <omer_azmon@intuit.com>
2017-09-21 16:04:15 -0700

    change based on reviewer comment

--- sceptre/stack.py
+++ sceptre/stack.py
@@ -196,9 +196,10 @@ class Stack(object):
             "Tags": [
                 {"Key": str(k), "Value": str(v)}
                 for k, v in self.config.get("stack_tags", {}).items()
-            ],
-            "OnFailure": self.config.get("on_failure", 'ROLLBACK')
+            ]
         }
+        if self.config.get("on_failure"):
+            self.config['OnFailure'] = self.config.get("on_failure") 
         create_stack_kwargs.update(self._get_template_details())
         create_stack_kwargs.update(self._get_role_arn())
         response = self.connection_manager.call(
