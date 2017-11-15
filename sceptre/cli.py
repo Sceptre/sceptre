387bdac8af698b976d7cdb19903584d9e0a50955
Niall Grant <ngfgrant@users.noreply.github.com>
2017-11-15 17:04:50 +0000

    Remove 'Response Metadata' from CLI output (#258)
    [Resolves #84] We need to improve the CLI output. The Response Metadata information
    returned from Boto clutters the success message when the
    validate-template command is called from the CLI.
    
    The validate template tests for the CLI  are also updated to reflect
    the expected changes in output. A new test is added to test invalid
    templates.

--- sceptre/cli.py
+++ sceptre/cli.py
@@ -155,8 +155,12 @@ def validate_template(ctx, environment, stack):
     Validates ENVIRONMENT/STACK's template.
     """
     env = get_env(ctx.obj["sceptre_dir"], environment, ctx.obj["options"])
-    result = env.stacks[stack].validate_template()
-    write(result, ctx.obj["output_format"])
+    response = env.stacks[stack].validate_template()
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        success_message = "Template is valid. Template details:\n"
+        response = _remove_response_metadata(response)
+        write(success_message, 'str')
+        write(response, ctx.obj["output_format"])
 
 
 @cli.command(name="generate-template")
@@ -828,6 +832,21 @@ def write(var, output_format="str", no_colour=True):
         stream = stack_status_colourer.colour(stream)
 
     click.echo(stream)
+
+
+def _remove_response_metadata(response):
+    """
+    Removes the Response Metadata from an AWS API reponse.
+
+    :param response: The AWS response that you wish to remove the Response \
+    Metadata from.
+    :type response: dict
+    :returns: The original dict with the Response Metadata removed.
+    :rtype: dict
+    """
+    if "ResponseMetadata" in response:
+        del response['ResponseMetadata']
+    return response
 
 
 class ColouredFormatter(Formatter):
