156afa0a4e99bb18b15aca974ae9b57fde30cbee
Niall Grant <ngfgrant@gmail.com>
2018-02-19 10:08:13 +0000

    Bump version: 1.3.3 → 1.3.4

--- sceptre/__init__.py
+++ sceptre/__init__.py
@@ -5,7 +5,7 @@ import logging
 
 __author__ = 'Cloudreach'
 __email__ = 'sceptre@cloudreach.com'
-__version__ = '1.3.3'
+__version__ = '1.3.4'
 
 
 # Set up logging to ``/dev/null`` like a library is supposed to.
