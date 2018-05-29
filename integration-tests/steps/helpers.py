36339107d9b51e9c1aa7144986e0b6af6eb57068
Juan Canham <Juan.Canham@cloudreach.co.uk>
2018-05-29 23:27:04 +0100

    fix linting exceptions

--- integration-tests/steps/helpers.py
+++ integration-tests/steps/helpers.py
@@ -26,13 +26,10 @@ def step_impl(context, message):
     else:
         raise Exception("Step has incorrect message")
 
-<<<<<<< HEAD
-=======
 
 @then('no exception is raised')
 def step_impl(context):
     assert (context.error is None)
->>>>>>> 4f71919... fixup linting
 
 
 @then('a "{exception_type}" is raised')
