7a4d6f839ab901e031866751cd5d130ce2e3b354
jfgiroux <giroux.jeanfrancois@gmail.com>
2018-05-30 11:29:50 -0400

    fixed missing new line

--- integration-tests/steps/stacks.py
+++ integration-tests/steps/stacks.py
@@ -8,6 +8,7 @@ from helpers import retry_boto_call
 
 from sceptre.config_reader import ConfigReader
 
+
 def set_stack_timeout(context, stack_name, stack_timeout):
     config_path = os.path.join(
         context.sceptre_dir, "config", stack_name + ".yaml"
@@ -19,6 +20,7 @@ def set_stack_timeout(context, stack_name, stack_timeout):
 
     with open(config_path, 'w') as config_file:
         yaml.safe_dump(stack_config, config_file, default_flow_style=False)
+
 
 @given('stack "{stack_name}" does not exist')
 def step_impl(context, stack_name):
@@ -71,9 +73,11 @@ def step_impl(context, stack_name, template_name):
     status = get_stack_status(context, full_name)
     assert (status == "CREATE_COMPLETE")
 
+
 @given('the stack_timeout for stack "{stack_name}" is "{stack_timeout}"')
 def step_impl(context, stack_name, stack_timeout):
     set_stack_timeout(context, stack_name, stack_timeout)
+
 
 @when('the user creates stack "{stack_name}"')
 def step_impl(context, stack_name):
