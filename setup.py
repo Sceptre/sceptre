d0544f8a1e9730bb027331b16a59cf987d93cfb3
Niall Grant <ngfgrant@gmail.com>
2019-01-07 10:08:36 +0000

    Update dependencies to fix CVE-2017-18342
    In PyYAML before 4.1, the yaml.load() API could execute arbitrary code.
    In other words, yaml.safe_load is not used.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("CHANGELOG.md") as history_file:
 install_requirements = [
     "boto3>=1.3,<2.0",
     "click==6.7",
-    "PyYaml==3.13",
+    "PyYaml>=4.2b1,<5.0",
     "Jinja2>=2.8,<3",
     "packaging==16.8",
     "colorama==0.3.7",
