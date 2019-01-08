891ca5bd5e7ba594f973ed5be48168619dd37681
Denis Astahov <adv4000@gmail.com>
2019-01-08 11:43:28 -0800

    Added support for CloudFormation Macros by adding CAPABILITY_AUTO_EXPAND
    CloudFormation Macros require Capability called CAPABILITY_AUTO_EXPAND.

--- sceptre/stack.py
+++ sceptre/stack.py
@@ -193,7 +193,7 @@ class Stack(object):
         create_stack_kwargs = {
             "StackName": self.external_name,
             "Parameters": self._format_parameters(self.parameters),
-            "Capabilities": ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM'],
+            "Capabilities": ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND'],
             "NotificationARNs": self.config.get("notifications", []),
             "Tags": [
                 {"Key": str(k), "Value": str(v)}
@@ -232,7 +232,7 @@ class Stack(object):
         update_stack_kwargs = {
             "StackName": self.external_name,
             "Parameters": self._format_parameters(self.parameters),
-            "Capabilities": ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM'],
+            "Capabilities": ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND'],
             "NotificationARNs": self.config.get("notifications", []),
             "Tags": [
                 {"Key": str(k), "Value": str(v)}
@@ -524,7 +524,7 @@ class Stack(object):
         create_change_set_kwargs = {
             "StackName": self.external_name,
             "Parameters": self._format_parameters(self.parameters),
-            "Capabilities": ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM'],
+            "Capabilities": ['CAPABILITY_IAM', 'CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND'],
             "ChangeSetName": change_set_name,
             "NotificationARNs": self.config.get("notifications", []),
             "Tags": [
