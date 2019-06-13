1f3aaab6430050886dede40db662d6bf3ddf7df2
Niall Grant <ngfgrant@gmail.com>
2019-06-13 11:23:44 +0100

    Update setup.py for Python3.7 support

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ install_requirements = [
 test_requirements = [
     "pytest>=3.2",
     "troposphere>=2.0.0",
-    "moto==0.4.31",
+    "moto==1.3.8",
     "mock==2.0.0",
     "behave==1.2.5",
-    "freezegun==0.3.9"
+    "freezegun==0.3.12"
 ]
 
 setup_requirements = [
@@ -85,6 +85,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.7"
     ],
     test_suite="tests",
     install_requires=install_requirements,
