82a8174fa9c94a5f87fcd7786a457e0597b45c15
Niall Grant <ngfgrant@gmail.com>
2019-07-08 13:21:57 +0100

    Add SceptreContext __repr__ test

--- sceptre/context.py
+++ sceptre/context.py
@@ -78,28 +78,38 @@ class SceptreContext(object):
     def __repr__(self):
         return ("sceptre.context.SceptreContext("
                 "project_path='{project_path}', "
-                "config_path='{config_path}', "
                 "command_path='{command_path}', "
-                "normal_command_path='{normal_command_path}', "
-                "config_file='{config_path}', "
-                "templates_path='{templates_path}', "
-                "user_variables='{user_variables}', "
-                "options='{options}', "
+                "user_variables={user_variables}, "
+                "options={options}, "
                 "output_format='{output_format}', "
-                "no_colour='{no_colour}', "
-                "ignore_dependencies='{ignore_dependencies}'".format(
+                "no_colour={no_colour}, "
+                "ignore_dependencies={ignore_dependencies})".format(
                     project_path=self.project_path,
-                    config_path=self.config_path,
                     command_path=self.command_path,
-                    normal_command_path=self.normal_command_path,
-                    config_file=self.config_file,
-                    templates_path=self.templates_path,
                     user_variables=self.user_variables,
                     options=self.options,
                     output_format=self.output_format,
                     no_colour=self.no_colour,
-                    ignore_dependencies=self.ignore_dependencies)
-                )
+                    ignore_dependencies=self.ignore_dependencies
+                ))
+
+    def __eq__(self, context):
+        return (
+            self.project_path == context.project_path
+            and self.config_path == context.config_path
+            and self.command_path == context.command_path
+            and self.normal_command_path == context.normal_command_path
+            and self.config_file == context.config_file
+            and self.templates_path == context.templates_path
+            and self.user_variables == context.user_variables
+            and self.options == context.options
+            and self.output_format == context.output_format
+            and self.no_colour == context.no_colour
+            and self.ignore_dependencies == context.ignore_dependencies
+        )
+
+    def __hash__(self):
+        return hash(str(self))
 
     def full_config_path(self):
         """
--- tests/test_context.py
+++ tests/test_context.py
@@ -1,17 +1,25 @@
 from os import path
 from mock import sentinel
+import importlib
+
 from sceptre.context import SceptreContext
 
 
 class TestSceptreContext(object):
 
     def setup_method(self, test_method):
-        self.templates_path = "templates"
-        self.config_path = "config"
-        self.config_file = "config.yaml"
+        self.context = SceptreContext(
+            project_path="project_path/to/sceptre",
+            command_path="command/path",
+            user_variables={},
+            options={},
+            output_format="json",
+            no_colour=True,
+            ignore_dependencies=False
+        )
 
     def test_context_with_path(self):
-        self.context = SceptreContext(
+        context = SceptreContext(
             project_path="project_path/to/sceptre",
             command_path="command-path",
             user_variables=sentinel.user_variables,
@@ -22,7 +30,7 @@ class TestSceptreContext(object):
         )
 
         sentinel.project_path = "project_path/to/sceptre"
-        assert self.context.project_path == sentinel.project_path
+        assert context.project_path == sentinel.project_path
 
     def test_full_config_path_returns_correct_path(self):
         context = SceptreContext(
@@ -35,7 +43,7 @@ class TestSceptreContext(object):
             ignore_dependencies=sentinel.ignore_dependencies
         )
 
-        full_config_path = path.join("project_path", self.config_path)
+        full_config_path = path.join("project_path", self.context.config_path)
         assert context.full_config_path() == full_config_path
 
     def test_full_command_path_returns_correct_path(self):
@@ -49,7 +57,7 @@ class TestSceptreContext(object):
             ignore_dependencies=sentinel.ignore_dependencies
         )
         full_command_path = path.join("project_path",
-                                      self.config_path,
+                                      self.context.config_path,
                                       "command")
 
         assert context.full_command_path() == full_command_path
@@ -64,5 +72,30 @@ class TestSceptreContext(object):
             no_colour=sentinel.no_colour,
             ignore_dependencies=sentinel.ignore_dependencies
         )
-        full_templates_path = path.join("project_path", self.templates_path)
+        full_templates_path = path.join("project_path", self.context.templates_path)
         assert context.full_templates_path() == full_templates_path
+
+    def test_stack_repr(self):
+        assert self.context.__repr__() == \
+            "sceptre.context.SceptreContext(" \
+            "project_path='project_path/to/sceptre', " \
+            "command_path='command/path', " \
+            "user_variables={}, "\
+            "options={}, " \
+            "output_format='json', " \
+            "no_colour=True, " \
+            "ignore_dependencies=False" \
+            ")"
+
+    def test_repr_can_eval_correctly(self):
+        sceptre = importlib.import_module('sceptre')
+        mock = importlib.import_module('mock')
+        evaluated_context = eval(
+            repr(self.context),
+            {
+                'sceptre': sceptre,
+                'sentinel': mock.mock.sentinel
+            }
+        )
+        assert isinstance(evaluated_context, SceptreContext)
+        assert evaluated_context.__eq__(self.context)
