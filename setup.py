fc0a291b251b1e3ebaa84a28a49b949d599da0db
Khai Do <zaro0508@gmail.com>
2021-04-15 10:49:36 -0700

    remove python 3.5 from supported list
    We currently only run tests on python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -82,7 +82,6 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Environment :: Console",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7"
     ],
