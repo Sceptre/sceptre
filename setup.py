5ee9cdeee234172a903e823ce6998cb7b858e098
Gabrielle Singh Cadieux <gabriellesinghcadieux+github@gmail.com>
2021-04-15 11:44:06 -0400

    [Resolves #978] Only install typing on Python<3.5 (#979)
    This PR constrains the installation of `typing` to Python<3.5, since it is included in the standard library as of 3.5 and causes issues if installed alongside the standard library version.

--- setup.py
+++ setup.py
@@ -19,8 +19,7 @@ install_requirements = [
     "colorama>=0.3.9",
     "packaging>=16.8,<17.0",
     "six>=1.11.0,<2.0.0",
-    "networkx==2.1",
-    "typing>=3.7.0,<3.8.0"
+    "networkx==2.1"
 ]
 
 test_requirements = [
