d3607b3d4a13c94394d0df55776fe6c47a0117ee
Yury Khalyavin <yury.khalyavin@gmail.com>
2021-05-26 16:53:13 +0000

    [Resolve #1050] Update click version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_version(rel_path):
 
 install_requirements = [
     "boto3>=1.3,<2.0",
-    "click>=7.0,<8.0",
+    "click>=7.0,<9.0",
     "PyYaml>=5.1,<6.0",
     "Jinja2>=2.8,<3",
     "colorama>=0.3.9",
