0d62fe39419fa94358e6b32d16f3c9d34bc3d2dc
Khai Do <zaro0508@gmail.com>
2021-09-22 09:15:59 -0700

    give sceptre-test-artifact a more random namee

--- integration-tests/environment.py
+++ integration-tests/environment.py
@@ -8,7 +8,7 @@ import random
 
 
 def before_all(context):
-    random_str = ''.join(random.choices(string.ascii_lowercase + string.digits, k=7))
+    random_str = ''.join(random.choices(string.ascii_lowercase + string.digits, k=16))
     context.TEST_ARTIFACT_BUCKET_NAME = f'sceptre-test-artifacts-{random_str}'
     context.region = boto3.session.Session().region_name
     context.uuid = uuid.uuid1().hex
