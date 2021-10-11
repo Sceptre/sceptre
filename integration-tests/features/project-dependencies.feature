2512be1ec42717190a882c1a8369586751f6a776
Jon Falkenstein <jfalkenstein@trueaccord.com>
2021-10-11 09:35:44 -0500

    testing integration tests

--- integration-tests/features/project-dependencies.feature
+++ integration-tests/features/project-dependencies.feature
@@ -4,4 +4,10 @@ Feature: Project Dependencies managed within Sceptre
     Given stack_group "managed-project-dependencies" does not exist
     And all files in template bucket for stack "managed-project-dependencies/resource" are deleted at cleanup
     When the user launches stack_group "managed-project-dependencies"
+    Then all the stacks in stack_group "managed-project-dependencies" are in "CREATE_COMPLETE"
 
+  Scenario: template bucket is managed in project
+    Given stack_group "managed-project-dependencies" does not exist
+    And all files in template bucket for stack "managed-project-dependencies/resource" are deleted at cleanup
+    When the user launches stack_group "managed-project-dependencies"
+    Then the template for stack "managed-project-dependencies/resource" has been uploaded
--- integration-tests/steps/project_dependencies.py
+++ integration-tests/steps/project_dependencies.py
@@ -1,5 +1,7 @@
+from itertools import chain
+
 import boto3
-from behave import given
+from behave import given, then
 from behave.runner import Context
 
 from sceptre.context import SceptreContext
@@ -8,11 +10,40 @@ from sceptre.plan.plan import SceptrePlan
 
 @given('all files in template bucket for stack "{stack_name}" are deleted at cleanup')
 def step_impl(context: Context, stack_name):
+
     context.add_cleanup(
         cleanup_template_files_in_bucket,
         context.sceptre_dir,
         stack_name
     )
+
+
+@then('the template for stack "{stack_name}" has been uploaded')
+def step_impl(context: Context, stack_name):
+    sceptre_context = SceptreContext(
+        command_path=stack_name + '.yaml',
+        project_path=context.sceptre_dir
+    )
+    plan = SceptrePlan(sceptre_context)
+    buckets = get_template_buckets(plan)
+    assert len(buckets) > 0
+    filtered_objects = list(chain.from_iterable(
+        bucket.objects.filter(
+            Prefix=stack_name
+        )
+        for bucket in buckets
+    ))
+
+    assert len(filtered_objects) == len(plan.command_stacks)
+    for stack in plan.command_stacks:
+        for obj in filtered_objects:
+            if obj.key.startswith(stack.name):
+                s3_template = obj.get()['Body'].read().decode('utf-8')
+                expected = stack.template.body
+                assert s3_template == expected
+                break
+        else:
+            assert False, "Could not found uploaded template"
 
 
 def cleanup_template_files_in_bucket(sceptre_dir, stack_name):
@@ -21,7 +52,15 @@ def cleanup_template_files_in_bucket(sceptre_dir, stack_name):
         project_path=sceptre_dir
     )
     plan = SceptrePlan(sceptre_context)
-    s3_resource = boto3.resource('s3')
-    for stack in plan.command_stacks:
-        bucket = s3_resource.Bucket(stack.template_bucket_name)
+    buckets = get_template_buckets(plan)
+    for bucket in buckets:
         bucket.objects.delete()
+
+
+def get_template_buckets(plan: SceptrePlan):
+    s3_resource = boto3.resource('s3')
+    return [
+        s3_resource.Bucket(stack.template_bucket_name)
+        for stack in plan.command_stacks
+        if stack.template_bucket_name is not None
+    ]
