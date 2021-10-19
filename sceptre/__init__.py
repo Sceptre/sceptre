64f538e12888b692b98d6b4085557d3dc171d5bb
Alex Harvey <alex.harvey@cmd.com.au>
2021-10-19 11:23:06 +1100

    Bump version: 2.11.0 → 2.12.0 (#30)

--- sceptre/__init__.py
+++ sceptre/__init__.py
@@ -6,7 +6,7 @@ import warnings
 
 __author__ = 'Cloudreach'
 __email__ = 'sceptre@cloudreach.com'
-__version__ = '2.11.0'
+__version__ = '2.12.0'
 
 
 # Set up logging to ``/dev/null`` like a library is supposed to.
