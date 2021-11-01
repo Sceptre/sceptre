08e056833a933150d17a3d9c6ba5bc269e2b9c00
Khai Do <zaro0508@gmail.com>
2021-11-01 10:41:31 -0700

    Fix from review
    * Better exceptions handling
    * Allow user to set custom timeout and retry options

--- sceptre/template_handlers/http.py
+++ sceptre/template_handlers/http.py
@@ -5,10 +5,17 @@ import tempfile
 import sceptre.template_handlers.helper as helper
 
 from requests.adapters import HTTPAdapter
+from requests.exceptions import InvalidURL, ConnectTimeout
 from requests.packages.urllib3.util.retry import Retry
 from sceptre.exceptions import UnsupportedTemplateFileTypeError
 from sceptre.template_handlers import TemplateHandler
 from urllib.parse import urlparse
+
+HANDLER_OPTION_KEY = "http_template_handler"
+HANDLER_RETRIES_OPTION_PARAM = "retries"
+DEFAULT_RETRIES_OPTION = 5
+HANDLER_TIMEOUT_OPTION_PARAM = "timeout"
+DEFAULT_TIMEOUT_OPTION = 5
 
 
 class Http(TemplateHandler):
@@ -43,8 +50,10 @@ class Http(TemplateHandler):
                 path.suffix, ",".join(self.supported_template_extensions)
             )
 
+        retries = self._get_handler_option(HANDLER_RETRIES_OPTION_PARAM, DEFAULT_RETRIES_OPTION)
+        timeout = self._get_handler_option(HANDLER_TIMEOUT_OPTION_PARAM, DEFAULT_TIMEOUT_OPTION)
         try:
-            template = self._get_template(url)
+            template = self._get_template(url, retries=retries, timeout=timeout)
             if path.suffix in self.jinja_template_extensions + self.python_template_extensions:
                 file = tempfile.NamedTemporaryFile(prefix=path.stem)
                 self.logger.debug("Template file saved to: %s", file.name)
@@ -68,28 +77,33 @@ class Http(TemplateHandler):
 
         return template
 
-    def _get_template(self, url):
+    def _get_template(self, url, retries, timeout):
         """
         Get template from the web
         :param url: The url to the template
         :type: str
-        :returns: The body of the CloudFormation template.
-        :rtype: str
+        :returns: The number of retry attempts.
+        :rtype: int
+        :returns: The timeout for the session in seconds.
+        :rtype: int
         """
         self.logger.debug("Downloading file from: %s", url)
-        session = self._get_retry_session()
+        session = self._get_retry_session(retries=retries)
         try:
-            response = session.get(url, timeout=(2, 5))
+            response = session.get(url, timeout=timeout)
             return response.content
-        except Exception as e:
-            self.logger.fatal(e)
+        except (InvalidURL, ConnectTimeout) as e:
             raise e
 
     def _get_retry_session(self,
-                           retries=5,
+                           retries,
                            backoff_factor=0.3,
                            status_forcelist=(429, 500, 502, 503, 504),
                            session=None):
+        """
+        Get a request session with retries.  Retry options are explained in the request libraries
+        https://urllib3.readthedocs.io/en/latest/reference/urllib3.util.html#module-urllib3.util.retry
+        """
         session = session or requests.Session()
         retry = Retry(
             total=retries,
@@ -102,3 +116,18 @@ class Http(TemplateHandler):
         session.mount('http://', adapter)
         session.mount('https://', adapter)
         return session
+
+    def _get_handler_option(self, name, default):
+        """
+        Get the template handler options
+        :param url: The option name
+        :type: str
+        :param default: The default value if option is not set.
+        :rtype: int
+        """
+        if HANDLER_OPTION_KEY in self.stack_group_config:
+            option = self.stack_group_config.get(HANDLER_OPTION_KEY)
+            if name in option:
+                return option.get(name)
+
+        return default
