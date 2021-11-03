Sceptre/sceptre

commit 5c0ab398b3a634336ce9dbd234b3d4b7d288ae8c

[Resolve #1135] Fix path to templates (#1141)
Currently the root file path for the template file handler points to a different
location than the existing template_path. This makes the migration process
confusing and arduous for users. Therefore when a user specifies a relative
path to the file we make both parameters point to the same location, the
`sceptre_project_dir/templates` directory.

sceptre/config/reader.py
@@ -568,7 +568,6 @@ class ConfigReader(object):
             for key in
             set(stack_group_config) - set(CONFIG_MERGE_STRATEGIES)
         }
-        parsed_config.pop("project_path")
         parsed_config.pop("stack_group_path")
         return parsed_config
 

sceptre/template_handlers/file.py
@@ -1,7 +1,7 @@
-import logging
 import os
 import sceptre.template_handlers.helper as helper
 
+from pathlib import Path
 from sceptre.exceptions import UnsupportedTemplateFileTypeError
 from sceptre.template_handlers import TemplateHandler
 
@@ -12,7 +12,6 @@ class File(TemplateHandler):
     """
 
     def __init__(self, *args, **kwargs):
-        self.logger = logging.getLogger(__name__)
         super(File, self).__init__(*args, **kwargs)
 
     def schema(self):
@@ -25,28 +24,33 @@ class File(TemplateHandler):
         }
 
     def handle(self):
-        file_extension = os.path.splitext(self.arguments["path"])[1]
-        path = self.arguments["path"]
+        project_path = self.stack_group_config.get("project_path")
+        input_path = Path(self.arguments["path"])
+        if input_path.is_absolute():
+            path = str(input_path)
+        else:
+            path = str(Path(project_path) / 'templates' / input_path)
+
+        if input_path.suffix not in self.supported_template_extensions:
+            raise UnsupportedTemplateFileTypeError(
+                "Template has file extension %s. Only %s are supported.",
+                input_path.suffix, ",".join(self.supported_template_extensions)
+            )
+
         try:
-            if file_extension in {".json", ".yaml", ".template"}:
+            if input_path.suffix in self.standard_template_extensions:
                 with open(path) as template_file:
                     return template_file.read()
-            elif file_extension == ".j2":
+            elif input_path.suffix in self.jinja_template_extensions:
                 return helper.render_jinja_template(
                     os.path.dirname(path),
                     os.path.basename(path),
                     {"sceptre_user_data": self.sceptre_user_data},
                     self.stack_group_config.get("j2_environment", {})
                 )
-            elif file_extension == ".py":
+            elif input_path.suffix in self.python_template_extensions:
                 return helper.call_sceptre_handler(path,
                                                    self.sceptre_user_data)
-            else:
-                raise UnsupportedTemplateFileTypeError(
-                    "Template has file extension %s. Only .py, .yaml, "
-                    ".template, .json and .j2 are supported.",
-                    os.path.splitext(path)[1]
-                )
         except Exception as e:
             helper.print_template_traceback(path)
             raise e

tests/test_config_reader.py
@@ -326,6 +326,7 @@ class TestConfigReader(object):
             template_bucket_name='stack_group_template_bucket_name',
             template_key_prefix=None,
             stack_group_config={
+                "project_path": self.context.project_path,
                 "custom_key": "custom_value"
             }
         )

tests/test_template.py
@@ -44,7 +44,9 @@ class TestTemplate(object):
             name="template_name",
             handler_config={"type": "file", "path": "/folder/template.py"},
             sceptre_user_data={},
-            stack_group_config={},
+            stack_group_config={
+                "project_path": "projects"
+            },
             connection_manager=connection_manager,
         )
 

commit 5608c1d42f561d5d9a5817c4f78f1729b58d3d77

[Resolves #1148] Correct path logic
This resolves a bug introduced in 5c0ab39. Note also that prior to
introduction of template handlers, a hack had existed to solve the
problem of computing the path when `--dir` is specified, see
https://github.com/Sceptre/sceptre/blob/354e87e8fbf2dafd2692c7c87abc1ecc35ea3f40/sceptre/template.py#L152-L167

In the case of --dir, the `File` handler class's `handle` method has
received a path relative to the `project_path` due to the previously
incorrectly-named method in the `ConfigReader` class
`_get_absolute_template_path` returning the relative path.

This patch renames the incorrectly-named method
`_get_absolute_template_path` as `_get_relative_template_path` and
corrects the logic in the `File` class's `handle` method.

sceptre/config/reader.py
@@ -531,7 +531,7 @@ class ConfigReader(object):
         stack = Stack(
             name=stack_name,
             project_code=config["project_code"],
-            template_path=self._get_absolute_template_path(config.get("template_path")),
+            template_path=self._get_relative_template_path(config.get("template_path")),
             template_handler_config=config.get("template"),
             region=config["region"],
             template_bucket_name=config.get("template_bucket_name"),
@@ -571,7 +571,7 @@ class ConfigReader(object):
         parsed_config.pop("stack_group_path")
         return parsed_config
 
-    def _get_absolute_template_path(self, template_path):
+    def _get_relative_template_path(self, template_path):
         if not template_path:
             return None
 

sceptre/template_handlers/file.py
@@ -26,10 +26,7 @@ class File(TemplateHandler):
     def handle(self):
         project_path = self.stack_group_config.get("project_path")
         input_path = Path(self.arguments["path"])
-        if input_path.is_absolute():
-            path = str(input_path)
-        else:
-            path = str(Path(project_path) / 'templates' / input_path)
+        path = str(input_path)
 
         if input_path.suffix not in self.supported_template_extensions:
             raise UnsupportedTemplateFileTypeError(