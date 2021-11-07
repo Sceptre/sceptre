25bd9578521352806d0a43468f83e2c15ab3beb7
Jon Falkenstein <jfalkenstein@trueaccord.com>
2021-11-07 15:46:10 -0600

    making template commands able to handle placeholders

--- sceptre/cli/template.py
+++ sceptre/cli/template.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 import click
 import webbrowser
 
@@ -7,13 +9,28 @@ from sceptre.cli.helpers import (
     write
 )
 from sceptre.plan.plan import SceptrePlan
+from sceptre.resolvers.placeholders import use_resolver_placeholders_on_error, PlaceholderType
+
+
+@contextmanager
+def null_context():
+    """A context manager that does nothing. This is identical to the nullcontext in py3.7+, but isn't
+    available in py3.6, so providing it here instead.
+    """
+    yield
 
 
 @click.command(name="validate", short_help="Validates the template.")
+@click.option(
+    '-n',
+    '--no-placeholders',
+    is_flag=True,
+    help="If True, placeholder values will be supplied for resolvers that cannot be resolved."
+)
 @click.argument("path")
 @click.pass_context
 @catch_exceptions
-def validate_command(ctx, path):
+def validate_command(ctx, no_placeholders, path):
     """
     Validates the template used for stack in PATH.
     \f
@@ -31,7 +48,14 @@ def validate_command(ctx, path):
     )
 
     plan = SceptrePlan(context)
-    responses = plan.validate()
+
+    if no_placeholders:
+        execution_context = null_context()
+    else:
+        execution_context = use_resolver_placeholders_on_error(PlaceholderType.alphanum)
+
+    with execution_context:
+        responses = plan.validate()
 
     for stack, response in responses.items():
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
@@ -41,10 +65,16 @@ def validate_command(ctx, path):
 
 
 @click.command(name="generate", short_help="Prints the template.")
+@click.option(
+    '-n',
+    '--no-placeholders',
+    is_flag=True,
+    help="If True, placeholder values will be supplied for resolvers that cannot be resolved."
+)
 @click.argument("path")
 @click.pass_context
 @catch_exceptions
-def generate_command(ctx, path):
+def generate_command(ctx, no_placeholders, path):
     """
     Prints the template used for stack in PATH.
     \f
@@ -62,16 +92,26 @@ def generate_command(ctx, path):
     )
 
     plan = SceptrePlan(context)
-    responses = plan.generate()
+
+    execution_context = null_context() if no_placeholders else use_resolver_placeholders_on_error()
+    with execution_context:
+        responses = plan.generate()
+
     output = [template for template in responses.values()]
     write(output, context.output_format)
 
 
 @click.command(name="estimate-cost", short_help="Estimates the cost of the template.")
+@click.option(
+    '-n',
+    '--no-placeholders',
+    is_flag=True,
+    help="If True, placeholder values will be supplied for resolvers that cannot be resolved."
+)
 @click.argument("path")
 @click.pass_context
 @catch_exceptions
-def estimate_cost_command(ctx, path):
+def estimate_cost_command(ctx, no_placeholders, path):
     """
     Prints a URI to STOUT that provides an estimated cost based on the
     resources in the stack. This command will also attempt to open a web
@@ -91,7 +131,13 @@ def estimate_cost_command(ctx, path):
     )
 
     plan = SceptrePlan(context)
-    responses = plan.estimate_cost()
+
+    if no_placeholders:
+        execution_context = null_context()
+    else:
+        execution_context = use_resolver_placeholders_on_error(PlaceholderType.alphanum)
+    with execution_context:
+        responses = plan.estimate_cost()
 
     for stack, response in responses.items():
         if response['ResponseMetadata']['HTTPStatusCode'] == 200:
