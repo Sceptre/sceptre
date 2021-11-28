7a9c2cee9b2f33b9c68f32dec38913bb6f2daf0c
Alex Harvey <alex.harvey@cmd.com.au>
2021-11-29 01:35:22 +1100

    Add missing output_format line

--- sceptre/cli/template.py
+++ sceptre/cli/template.py
@@ -157,6 +157,7 @@ def detect_stack_drift_command(ctx, path):
         project_path=ctx.obj.get("project_path"),
         user_variables=ctx.obj.get("user_variables"),
         options=ctx.obj.get("options"),
+        output_format=ctx.obj.get("output_format"),
         ignore_dependencies=ctx.obj.get("ignore_dependencies")
     )
 
