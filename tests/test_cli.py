e52821753e88e9596a033ab637cf5bef3ac2a9e4
Alex Harvey <alex.harvey@cmd.com.au>
2022-01-04 22:33:54 +1100

    Satisfy linter

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1010,7 +1010,14 @@ class TestCli(object):
             cli, ["drift", "detect", "dev/vpc.yaml"]
         )
         assert result.exit_code == 0
-        assert result.output == 'fake_stack:\n  DetectionStatus: DETECTION_COMPLETE\n  DriftedStackResourceCount: 0\n  StackDriftDetectionId: 3fb76910-f660-11eb-80ac-0246f7a6da62\n  StackDriftStatus: IN_SYNC\n  StackId: fake-stack-id\n\n'
+        assert result.output == (
+            'fake_stack:\n'
+            '  DetectionStatus: DETECTION_COMPLETE\n'
+            '  DriftedStackResourceCount: 0\n'
+            '  StackDriftDetectionId: 3fb76910-f660-11eb-80ac-0246f7a6da62\n'
+            '  StackDriftStatus: IN_SYNC\n'
+            '  StackId: fake-stack-id\n\n'
+        )
 
     def test_drift_show(self):
         self.mock_stack_actions.drift_show.return_value = (
