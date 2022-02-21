b0a7a963a7ced7e4cad6bb0c8bdee8c19df96f31
Khai Do <zaro0508@gmail.com>
2022-02-20 22:22:58 -0800

    drop support for python 3.6

--- setup.py
+++ setup.py
@@ -96,7 +96,6 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Environment :: Console",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
