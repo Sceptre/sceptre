0fce8989aacfddbfeb562d738869b14839a7fd21
Jon Falkenstein <jfalkenstein@trueaccord.com>
2022-02-22 10:09:01 -0600

    bumping version

--- sceptre/__init__.py
+++ sceptre/__init__.py
@@ -6,7 +6,7 @@ import warnings
 
 __author__ = 'Cloudreach'
 __email__ = 'sceptre@cloudreach.com'
-__version__ = '2.7.1'
+__version__ = '3.0.0'
 
 
 # Set up logging to ``/dev/null`` like a library is supposed to.
