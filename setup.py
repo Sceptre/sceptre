d580246a1edc4e1f10311bf5899c7b1163ff7845
Khai Do <zaro0508@gmail.com>
2022-05-26 08:22:43 -0700

    add documentation

--- setup.py
+++ setup.py
@@ -99,6 +99,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     test_suite="tests",
     install_requires=install_requirements,
