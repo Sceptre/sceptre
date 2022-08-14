eaf9662fdc2d5129c29bc23aef4ccf434d9b87b8
Jon Falkenstein <jfalkenstein@trueaccord.com>
2022-08-14 09:31:31 -0500

    small docs improvement

--- sceptre/plan/executor.py
+++ sceptre/plan/executor.py
@@ -35,7 +35,7 @@ class SceptrePlanExecutor(object):
 
     def execute(self, *args):
         """
-        Execute is responsible executing the sets of Stacks in ``launch_order``
+        Execute is responsible executing the sets of Stacks in launch_order
         concurrently, in the correct order.
 
         :param args: Any arguments that should be passed through to the
