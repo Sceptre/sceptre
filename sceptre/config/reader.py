2330e770e9cde3fd4edf4f4bd54205de35d8dbd0
Simon Heather <32168619+X-Guardian@users.noreply.github.com>
2023-01-20 16:27:52 +0000

    Update sceptre/config/reader.py
    Co-authored-by: Jon Falkenstein <77296393+jfalkenstein@users.noreply.github.com>

--- sceptre/config/reader.py
+++ sceptre/config/reader.py
@@ -449,7 +449,7 @@ class ConfigReader(object):
             except Exception as err:
                 raise SceptreException(
                     f"{Path(directory_path, basename).as_posix()} - {err}"
-                )
+                ) from err
 
             try:
                 config = yaml.safe_load(rendered_template)
