9258576606aa02d3fcddf96e2f7e3dbe9e951aad
Jon Falkenstein <jfalkenstein@trueaccord.com>
2023-02-18 12:49:27 -0600

    adding docstring

--- sceptre/logging.py
+++ sceptre/logging.py
@@ -4,6 +4,12 @@ from typing import MutableMapping, Any, Tuple
 
 class StackLoggerAdapter(LoggerAdapter):
     def __init__(self, logger: Logger, stack_name: str, extra: dict = None):
+        """A small wrapper around a Logger that prefixes log messages with the stack name.
+
+        :param logger: The logger to wrap
+        :param stack_name: The name of the stack to every log message
+        :param extra: Extra kwargs to add to the log context (if any)
+        """
         super().__init__(logger, extra or {})
         self.stack_name = stack_name
 
