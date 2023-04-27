22a0516f66182f96961c1dee17e52553a33f52d0
Alex Harvey <alexharv074@gmail.com>
2023-04-27 21:34:32 +1000

    Update sceptre/cli/template.py
    Co-authored-by: Jon Falkenstein <77296393+jfalkenstein@users.noreply.github.com>

--- sceptre/cli/template.py
+++ sceptre/cli/template.py
@@ -67,7 +67,7 @@ def validate_command(ctx, no_placeholders, path):
 @click.argument("path")
 @click.pass_context
 @catch_exceptions
-@deprecation.deprecated("4.0.0", "6.0.0", __version__, "Use dump template instead.")
+@deprecation.deprecated("4.2.0", "5.0.0", __version__, "Use dump template instead.")
 def generate_command(ctx, no_placeholders, path):
     """
     Prints the template used for stack in PATH.
