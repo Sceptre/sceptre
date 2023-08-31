2bc252ed22ee0fbb21a57524061f2e4c42be9e36
Iain Samuel McLean Elder <iain@isme.es>
2023-08-31 13:14:20 +0200

    Use pytest fixtures

--- tests/test_hooks/test_cmd.py
+++ tests/test_hooks/test_cmd.py
@@ -8,105 +8,104 @@ from sceptre.hooks.cmd import Cmd
 from sceptre.stack import Stack
 
 
-class TestCmd(object):
-    def setup_method(self, test_method):
-        self.stack = Stack(
-            "stack1",
-            "project1",
-            "region1",
-            template_handler_config={"template": "path.yaml"},
-        )
+@pytest.fixture()
+def stack():
+    stack = Stack(
+        "stack1",
+        "project1",
+        "region1",
+        template_handler_config={"template": "path.yaml"},
+    )
 
-        # Otherwise the test works only when the environment variables already
-        # set a valid AWS session.
-        self.stack.connection_manager.create_session_environment_variables = Mock(
-            return_value={}
-        )
+    # Otherwise the test works only when the environment variables already set a
+    # valid AWS session.
+    stack.connection_manager.create_session_environment_variables = Mock(
+        return_value={}
+    )
 
-        self.cmd = Cmd(stack=self.stack)
+    return stack
 
-    def test_null_input_raises_exception(self):
-        self.cmd.argument = None
-        with pytest.raises(InvalidHookArgumentTypeError):
-            self.cmd.run()
 
-    def test_empty_string_raises_exception(self):
-        self.cmd.argument = ""
-        with pytest.raises(InvalidHookArgumentTypeError):
-            self.cmd.run()
+def test_null_input_raises_exception(stack):
+    with pytest.raises(InvalidHookArgumentTypeError):
+        Cmd(None, stack).run()
 
-    def test_list_raises_exception(self):
-        self.cmd.argument = ["echo", "hello"]
-        with pytest.raises(InvalidHookArgumentTypeError):
-            self.cmd.run()
 
-    def test_dict_without_executable_raises_exception(self):
-        self.cmd.argument = {"args": "echo hello"}
-        with pytest.raises(InvalidHookArgumentTypeError):
-            self.cmd.run()
+def test_empty_string_raises_exception(stack):
+    with pytest.raises(InvalidHookArgumentTypeError):
+        Cmd("", stack).run()
 
-    def test_dict_without_args_raises_exception(self):
-        self.cmd.argument = {"executable": "/bin/bash"}
-        with pytest.raises(InvalidHookArgumentTypeError):
-            self.cmd.run()
 
-    def test_dict_with_list_args_raises_exception(self):
-        self.cmd.argument = {"args": ["echo", "hello"], "executable": "/bin/bash"}
-        with pytest.raises(InvalidHookArgumentTypeError):
-            self.cmd.run()
+def test_list_raises_exception(stack):
+    with pytest.raises(InvalidHookArgumentTypeError):
+        Cmd(["echo", "hello"], stack).run()
 
-    def test_dict_with_list_executable_raises_exception(self):
-        self.cmd.argument = {"args": "echo hello", "executable": ["/bin/bash"]}
-        with pytest.raises(InvalidHookArgumentTypeError):
-            self.cmd.run()
 
-    def test_input_exception_reprs_input(self):
-        import datetime
+def test_dict_without_executable_raises_exception(stack):
+    with pytest.raises(InvalidHookArgumentTypeError):
+        Cmd({"args": "echo hello"}, stack).run()
 
-        self.cmd.argument = datetime.date(2023, 8, 31)
-        exception_message = (
-            r"A cmd hook requires either a string argument or an object with "
-            r"args and executable keys with string values\. You gave "
-            r"datetime.date\(2023, 8, 31\)\."
-        )
-        with pytest.raises(InvalidHookArgumentTypeError, match=exception_message):
-            self.cmd.run()
 
-    def test_zero_exit_returns(self):
-        self.cmd.argument = "exit 0"
-        self.cmd.run()
+def test_dict_without_args_raises_exception(stack):
+    with pytest.raises(InvalidHookArgumentTypeError):
+        Cmd({"executable": "/bin/bash"}, stack).run()
 
-    def test_nonzero_exit_raises_exception(self):
-        self.cmd.argument = "exit 1"
-        with pytest.raises(CalledProcessError):
-            self.cmd.run()
 
-    def test_command_writes_to_stdout(self, capfd):
-        self.cmd.argument = "echo hello"
-        self.cmd.run()
+def test_dict_with_list_args_raises_exception(stack):
+    with pytest.raises(InvalidHookArgumentTypeError):
+        Cmd({"args": ["echo", "hello"], "executable": "/bin/bash"}, stack).run()
+
+
+def test_dict_with_list_executable_raises_exception(stack):
+    with pytest.raises(InvalidHookArgumentTypeError):
+        Cmd({"args": "echo hello", "executable": ["/bin/bash"]}, stack).run()
+
+
+def test_input_exception_reprs_input(stack):
+    import datetime
+
+    exception_message = (
+        r"A cmd hook requires either a string argument or an object with args and "
+        r"executable keys with string values\. You gave datetime.date\(2023, 8, 31\)\."
+    )
+    with pytest.raises(InvalidHookArgumentTypeError, match=exception_message):
+        Cmd(datetime.date(2023, 8, 31), stack).run()
+
+
+def test_zero_exit_returns(stack):
+    Cmd("exit 0", stack).run()
+
+
+def test_nonzero_exit_raises_exception(stack):
+    with pytest.raises(CalledProcessError):
+        Cmd("exit 1", stack).run()
+
+
+def test_command_writes_to_stdout(stack, capfd):
+    Cmd("echo hello", stack).run()
+    cap = capfd.readouterr()
+    assert cap.out.strip() == "hello"
+    assert cap.err.strip() == ""
+
+
+def test_shell_error_writes_to_stderr(stack, capfd):
+    try:
+        Cmd("missing_command", stack).run()
+    except CalledProcessError:
         cap = capfd.readouterr()
-        assert cap.out.strip() == "hello"
-        assert cap.err.strip() == ""
+        assert cap.out.strip() == ""
+        assert "missing_command: not found" in cap.err
 
-    def test_shell_error_writes_to_stderr(self, capfd):
-        self.cmd.argument = "missing_command"
-        try:
-            self.cmd.run()
-        except CalledProcessError:
-            cap = capfd.readouterr()
-            assert cap.out.strip() == ""
-            assert "missing_command: not found" in cap.err
 
-    def test_default_shell_is_sh(self, capfd):
-        self.cmd.argument = "echo $0"
-        self.cmd.run()
-        cap = capfd.readouterr()
-        assert cap.out.strip() == "/bin/sh"
-        assert cap.err.strip() == ""
+def test_default_shell_is_sh(stack, capfd):
+    Cmd("echo $0", stack).run()
+    cap = capfd.readouterr()
+    assert cap.out.strip() == "/bin/sh"
+    assert cap.err.strip() == ""
 
-    def test_executable_sets_the_shell(self, capfd):
-        self.cmd.argument = {"args": "echo $0", "executable": "/bin/bash"}
-        self.cmd.run()
-        cap = capfd.readouterr()
-        assert cap.out.strip() == "/bin/bash"
-        assert cap.err.strip() == ""
+
+def test_executable_sets_the_shell(stack, capfd):
+    Cmd({"args": "echo $0", "executable": "/bin/bash"}, stack).run()
+    cap = capfd.readouterr()
+    assert cap.out.strip() == "/bin/bash"
+    assert cap.err.strip() == ""
