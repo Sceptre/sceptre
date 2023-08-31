d578c1fbc28cd48c4ea5b1dcb25fc73958c89dcd
Iain Samuel McLean Elder <iain@isme.es>
2023-08-31 12:55:24 +0200

    Cover all test cases I can think of

--- sceptre/hooks/cmd.py
+++ sceptre/hooks/cmd.py
@@ -20,14 +20,22 @@ class Cmd(Hook):
         """
         envs = self.stack.connection_manager.create_session_environment_variables()
 
-        if self.argument is None:
-            raise InvalidHookArgumentTypeError()
-
-        if isinstance(self.argument, str) or self.argument is None:
+        if isinstance(self.argument, str) and self.argument != "":
             args = self.argument
             executable = None
-        elif isinstance(self.argument, dict):
+        elif (
+            isinstance(self.argument, dict)
+            and set(self.argument) == {"args", "executable"}
+            and isinstance(self.argument["args"], str)
+            and isinstance(self.argument["executable"], str)
+        ):
             args = self.argument["args"]
             executable = self.argument["executable"]
+        else:
+            raise InvalidHookArgumentTypeError(
+                "A cmd hook requires either a string argument or an object with "
+                "args and executable keys with string values. "
+                f"You gave {self.argument!r}."
+            )
 
         subprocess.check_call(args, shell=True, env=envs, executable=executable)
--- tests/test_hooks/test_cmd.py
+++ tests/test_hooks/test_cmd.py
@@ -25,25 +25,88 @@ class TestCmd(object):
 
         self.cmd = Cmd(stack=self.stack)
 
-    def test_run_with_null(self):
+    def test_null_input_raises_exception(self):
         self.cmd.argument = None
         with pytest.raises(InvalidHookArgumentTypeError):
             self.cmd.run()
 
-    def test_run_with_correct_command(self, capfd):
+    def test_empty_string_raises_exception(self):
+        self.cmd.argument = ""
+        with pytest.raises(InvalidHookArgumentTypeError):
+            self.cmd.run()
+
+    def test_list_raises_exception(self):
+        self.cmd.argument = ["echo", "hello"]
+        with pytest.raises(InvalidHookArgumentTypeError):
+            self.cmd.run()
+
+    def test_dict_without_executable_raises_exception(self):
+        self.cmd.argument = {"args": "echo hello"}
+        with pytest.raises(InvalidHookArgumentTypeError):
+            self.cmd.run()
+
+    def test_dict_without_args_raises_exception(self):
+        self.cmd.argument = {"executable": "/bin/bash"}
+        with pytest.raises(InvalidHookArgumentTypeError):
+            self.cmd.run()
+
+    def test_dict_with_list_args_raises_exception(self):
+        self.cmd.argument = {"args": ["echo", "hello"], "executable": "/bin/bash"}
+        with pytest.raises(InvalidHookArgumentTypeError):
+            self.cmd.run()
+
+    def test_dict_with_list_executable_raises_exception(self):
+        self.cmd.argument = {"args": "echo hello", "executable": ["/bin/bash"]}
+        with pytest.raises(InvalidHookArgumentTypeError):
+            self.cmd.run()
+
+    def test_input_exception_reprs_input(self):
+        import datetime
+
+        self.cmd.argument = datetime.date(2023, 8, 31)
+        exception_message = (
+            r"A cmd hook requires either a string argument or an object with "
+            r"args and executable keys with string values\. You gave "
+            r"datetime.date\(2023, 8, 31\)\."
+        )
+        with pytest.raises(InvalidHookArgumentTypeError, match=exception_message):
+            self.cmd.run()
+
+    def test_zero_exit_returns(self):
+        self.cmd.argument = "exit 0"
+        self.cmd.run()
+
+    def test_nonzero_exit_raises_exception(self):
+        self.cmd.argument = "exit 1"
+        with pytest.raises(CalledProcessError):
+            self.cmd.run()
+
+    def test_command_writes_to_stdout(self, capfd):
         self.cmd.argument = "echo hello"
         self.cmd.run()
         cap = capfd.readouterr()
         assert cap.out.strip() == "hello"
         assert cap.err.strip() == ""
 
-    def test_run_with_erroring_command(self):
-        self.cmd.argument = "missing_command_that_causes_shell_error"
-        with pytest.raises(CalledProcessError):
+    def test_shell_error_writes_to_stderr(self, capfd):
+        self.cmd.argument = "missing_command"
+        try:
             self.cmd.run()
+        except CalledProcessError:
+            cap = capfd.readouterr()
+            assert cap.out.strip() == ""
+            assert "missing_command: not found" in cap.err
 
-    def test_run_with_command_and_executable(self):
-        # TODO: Choose how to test this.
-        # Mock subprocess and check that executable message is sent?
-        # Spy Popen and check executable?
-        pytest.fail()
+    def test_default_shell_is_sh(self, capfd):
+        self.cmd.argument = "echo $0"
+        self.cmd.run()
+        cap = capfd.readouterr()
+        assert cap.out.strip() == "/bin/sh"
+        assert cap.err.strip() == ""
+
+    def test_executable_sets_the_shell(self, capfd):
+        self.cmd.argument = {"args": "echo $0", "executable": "/bin/bash"}
+        self.cmd.run()
+        cap = capfd.readouterr()
+        assert cap.out.strip() == "/bin/bash"
+        assert cap.err.strip() == ""
